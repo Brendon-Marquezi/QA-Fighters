d4a5375a97a8596ddbfc3211aa8caa9d367ea11a
erik <erikbarros1402@gmail.com>
2024-07-29 15:08:40 -0300

    update-feature-project

---
+++ tests/011-Checkwhetheranewprojectcanbecreatedsuccessfully.test.js
@@ -0,0 +1,28 @@
+const env = require('#configs/environments');
+const RequestManager = require('#utils/requestManager');
+
+const requestManager = new RequestManager(env.environment.base_url);
+
+const basicAuth =
+  'Basic ' +
+  Buffer.from(
+    `${env.environment.username}:${env.environment.api_token}`,
+  ).toString('base64');
+
+test('Check whether a new project can be created successfully', async () => {
+  const projectResponse = await requestManager.send(
+    'post',
+    'project',
+    {},
+    { Authorization: `${basicAuth}` },
+    {                                               
+      key: 'TESTE02',
+      name: 'Example09',
+      projectTemplateKey: 'com.pyxis.greenhopper.jira:gh-simplified-scrum-classic',
+      leadAccountId: env.environment.leadAccountId,
+    },
+  );
+
+  expect(projectResponse.status).toBe(201);
+  expect(projectResponse.data.key).toBe('TESTE02');
+});

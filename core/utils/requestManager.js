2fb280a468a2d6c454ae04dd1e92f5f5e072fe5a
queilaFernandes <Queila.Fernandes@jala.university>
2024-08-03 19:21:20 -0300

    Fix:Sending the tests of the groups functionality with the beforeEach and afterEach added that were missing. And with the context variables like group_name defined in the context variables

--- core/utils/requestManager.js
+++ core/utils/requestManager.js
@@ -3,11 +3,15 @@ const axios = require('axios');
 
 class RequestManager {
   constructor(baseURL, headers = {}, timeout = env.configuration.timeout) {
-    this.axios = axios.create({
-      baseURL,
-      headers,
-      timeout,
-    });
+    if (!RequestManager.instance) {
+      this.axios = axios.create({
+        baseURL,
+        headers,
+        timeout,
+      });
+      RequestManager.instance = this;
+    }
+    return RequestManager.instance;
   }
 
   async send(method, endpoint, params, headers, data) {
